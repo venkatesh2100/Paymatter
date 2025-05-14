a24a8fbc8c1ea7b033e4218a854f9fa84a01ed36
venky <venky@venky.kali>
2025-05-14 21:51:31 +0530

    Remodel

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -4,22 +4,6 @@ import Navbar from "../components/navbar";
 export * from "@prisma/client";
 import HomePage from "../components/Homepage";
 export default function Home() {
-  /* useState & useEffect code.
-  const [count ,setcount] = useState(0);
-  useEffect(()=>{
-    if(code(count)){
-      setTimeout(()=>{
-        setcount(count=>count+1)
-      },1000)
-    }
-  },[count])
-
-  function code(i:any) {
-    if(i===10){
-      return false;
-    }
-    return true
-  }*/
 
   return (
     <div>
