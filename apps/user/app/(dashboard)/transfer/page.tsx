3125dfc827766d941bdb0caf965a1593a4c16797
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-06-09 20:27:18 +0530

    Fixed Balance Creation.

--- apps/user/app/(dashboard)/transfer/page.tsx
+++ apps/user/app/(dashboard)/transfer/page.tsx
@@ -8,56 +8,56 @@ import prisma from "@repo/db/client";
 
 
 async function getBalance() {
-    const session = await getServerSession(authOptions);
-    // console.log(session);
+  const session = await getServerSession(authOptions);
+  // console.log(session);
 
-    const balance = await prisma.balance.findFirst({
-        where: {
-            userId: Number(session?.user?.id)
-        }
-    });
-    return {
-        amount: balance?.amount || 0,
-        locked: balance?.locked || 0
+  const balance = await prisma.balance.findFirst({
+    where: {
+      userId: Number(session?.user?.id)
     }
+  });
+  return {
+    amount: balance?.amount || 0,
+    locked: balance?.locked || 0
+  }
 }
 // console.log(balance)
 
 async function getOnRampTransactions() {
-    const session = await getServerSession(authOptions);
-    const txns = await prisma.onRamptransactions.findMany({
-        where: {
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.onRamptransactions.findMany({
+    where: {
 
-            userId: Number(session?.user?.id)
-        }
-    });
-    return txns.map(t => ({
-        time: t.startTime,
-        amount: t.amount,
-        status: t.status,
-        provider: t.provider
-    }))
+      userId: Number(session?.user?.id)
+    }
+  });
+  return txns.map(t => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider
+  }))
 }
 
 // eslint-disable-next-line react/display-name
-export default async function(){
-    const balance = await getBalance();
-    const transactions = await getOnRampTransactions();
+export default async function () {
+  const balance = await getBalance();
+  const transactions = await getOnRampTransactions();
 
-    return <div className="w-6xl">
-        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
-            Transfer
-        </div>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4 md:gap-10">
-            <div>
-                <AddMoney />
-            </div>
-            <div>
-                <BalanceCard amount={balance.amount} locked={balance.locked} />
-                <div className="pt-4">
-                    <OnRampTransactions transactions={transactions} />
-                </div>
-            </div>
-        </div>
+  return <div className="w-10xl">
+    <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+      Transfer
     </div>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4 md:gap-10">
+      <div>
+        <AddMoney />
+      </div>
+      <div>
+        <BalanceCard amount={balance.amount} locked={balance.locked} />
+        <div className="pt-4">
+          <OnRampTransactions transactions={transactions} />
+        </div>
+      </div>
+    </div>
+  </div>
 }
--- apps/user/app/api/signup/route.ts
+++ apps/user/app/api/signup/route.ts
@@ -2,10 +2,12 @@
 import { NextResponse } from "next/server";
 import db from "@repo/db/client";
 import bcrypt from "bcrypt";
+import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
   const { username, phonenumber, password } = await req.json();
-  console.log("kjfkldsjk;j")
+  // console.log("kjfkldsjk;j")
+  await getServerSession();
   if (!username || !phonenumber || !password) {
     return NextResponse.json({ error: "Missing fields" }, { status: 400 });
   }
@@ -26,19 +28,28 @@ export async function POST(req: Request) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   // console.log('user ckfjsdkfjskl;df')
-try {
-  const user = await db.user.create({
-    data: {
-      username,
-      phonenumber,
-      password: hashedPassword,
-    },
-  });
-  // console.log(user);
-  return NextResponse.json({ message: "User created", user });
-} catch (error) {
-  console.error("Error creating user:", error);
-  return NextResponse.json({ error: "Failed to create user" }, { status: 500 });
-}
+  try {
+    const user = await db.user.create({
+      data: {
+        username,
+        phonenumber,
+        password: hashedPassword,
+      },
+    });
+
+    await db.balance.create({
+      data: {
+        amount: 0,
+        locked: 0,
+        userId: user.id,
+
+      }
+    })
+    // console.log(user);
+    return NextResponse.json({ message: "User created", user });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json({ error: "Failed to create user" }, { status: 500 });
+  }
 
 }
